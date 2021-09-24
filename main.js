7f47321105b4fee3a19cc9009aea779e039b22ee
shineyjain <86151792+shineyjain@users.noreply.github.com>
2021-09-24 14:07:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function addUser() {
+player1_name =document.getElementById("Player1Name").value;
+player2_name =document.getElementById("Player2Name").value;
+localStorage.setItem("player1_name",player1_name);
+localStorage.setItem("player2_name",player2_name);
+window.location ="game_page.html";
+}
